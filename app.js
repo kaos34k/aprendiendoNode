9a2fcd9e7d4cee91ce4cce69da9bfb738298464b
kaos34k <kaos34@gmail.com>
2016-03-22 14:12:44 -0500

    s

--- app.js
+++ app.js
@@ -8,23 +8,23 @@ var app = sever();
 * Preparaqmos el motor de plantillas jade 
 */
 app.set( "view engine", "jade" );
-
+//es para crear una ruta donde se serviran las imagene, los stilos css entre otros.
+app.use(sever.static('public'));
 
 /*
 * Metodo get enviamos datos a una vista index.jade
 */
 app.get("/",function(req, res){
-	res.render( "index", { hola: "yeison"} );
-	res.end();
+	res.render("index");
 });
 
 /*
 * Metodo post que recibe parametros de un formulario
 */
-
+/*
 app.post("/", function( req, res ){
 	res.render("form");
-}); 
+}); */
 
 /*
 *Especificamos el puerto
