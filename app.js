0997f0be39d4d089b6f37f8bae0b7e255c2e9948
kaos34k <kaos34@gmail.com>
2016-03-26 13:07:48 -0500

    editar eliminar, falta rofundizar en eso  ya lista

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
-
-
-//*Crear servidor con un objeto app 
+/*
+*Estylos para un over redondo: http://tympanus.net/codrops/2012/08/08/circle-hover-effects-with-css-transitions/
+*Comandos para instalar dependencias : -->
+* npm install express --save //--crear el servidor 
+* npm install mongoose --save //bases de datos
+* npm install body-parser --save //--parsear eljson de resultados
+* npm install multer --save //--para subir archivos 
+* npm install cloudinary --save //--un repositorio para subir archivos
+*/
+//Crear servidor con un objeto app 
 var sever = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var multer  = require('multer');
+var cloudinary = require('cloudinary');
+var methodOverride = require('method-override');
+var pwd = "1234";
+
+//Configurar cloudinary
+cloudinary.config({
+	cloud_name:"dgevxnhnn",
+	api_key: "396979134749836",
+	api_secret: "zn4oGtIVoJF_1TUZS6yiWqD7baQ"
+});
+
 var app = sever();
 //es para crear una ruta donde se serviran las imagene, los stilos css entre otros.
 app.use(sever.static('public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded( {extended: true} ) );
 
+//Funciona para poder comprender el body o cuerpo de la información
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+//app.use(multer({dest:"./uploads/"})); No se por que esta dañado no funciona
 
 /*
 *Crear una base de datos en la consola de mongo = use nombre de la db
 * conectar a la base de datos mongoose.connect("mongodb://localhost/webNode");
-* >>>>>>>>>>PILAS aqui esta la solución por si no te puedes conectar a mongo---------http://dba.stackexchange.com/questions/55056/how-to-connect-to-mongodb-on-windows-8
+* >>>>PILAS aqui esta la solución por si no te puedes conectar a mongo---------http://dba.stackexchange.com/questions/55056/how-to-connect-to-mongodb-on-windows-8
 */
 mongoose.connect("mongodb://localhost/webNode");
 //Definir un esquema o una tabla.
@@ -26,36 +48,133 @@ var productoChema = {
 };
 
 var Producto = mongoose.model("Producto", productoChema );
+
 // Preparaqmos el motor de plantillas jade 
 app.set( "view engine", "jade" );
-
-
 
 //* Metodo get enviamos datos a una vista index.jade
 app.get("/",function(req, res){
 	res.render("index");
 });
 
-app.post("/menu", function( solicitud, respuesta ){
+//Consultar o listar productos
+app.get("/menu", function( solicitud, respuesta ) {
+	Producto.find(function(error, documento) {
+		if(error){console.log(error)}
+		respuesta.render("menu/index", { productos: documento });	
+	});
+});
+
+//Formulario de ingreso a la app
+app.post("/admin",function(solicitud,respuesta){
+	if(solicitud.body.password == pwd){
+		Producto.find(function(error,documento){
+			if(error){ console.log(error); }
+			respuesta.render("admin/index",{ productos: documento })
+		});
+	}else{
+		respuesta.redirect("/");
+	}
+});
+
+app.get("/admin",function(solicitud,respuesta){
+	respuesta.render("admin/form");
+});
+ 
+
+app.post("/menu/:id",function(solicitud,respuesta){	
+	if(solicitud.body.password == pwd){
+		var data = {
+			title: solicitud.body.title,
+			description: solicitud.body.description,
+			pricing: solicitud.body.pricing
+		};
+
+		Producto.update({"_id": solicitud.params.id}, data, function(product){
+			respuesta.redirect("/menu");
+		});	
+
+	} else {
+		respuesta.redirect("/");
+	}
+});
+
+app.get("/menu/edit/:id",function(solicitud,respuesta){
+	var id_producto = solicitud.params.id;
+	Producto.findOne({"_id": id_producto},function(error,producto){
+		respuesta.render("menu/edit",{ product: producto });
+	});
+
+});
+
+
+/*/Editar un producto 
+app.put("/menu/:id", function(solicitud,respuesta) {
 	var data = {
 		title: solicitud.body.title,
 		descripcion: solicitud.body.descripcion,
-		imagenUrl: solicitud.body.imagenUrl,
+		precio: solicitud.body.precio
+	};
+	console.log(solicitud.params.id);
+	Producto.update( {"_id": solicitud.params.id}, data, function(producto){
+		respuesta.redirect("/menu");
+	});
+});
+
+app.get("/menu/edit/:id", function(solicitud, respuesta){
+	var id_producto = solicitud.params.id;
+	Producto.findOne({"_id": id_producto }, function(error, producto){
+		respuesta.render("menu/edit", { producto: producto });
+	});	
+});
+*/
+
+//pasar la vista del menu registro de productos
+app.get("/menu/new", function( solicitud, respuesta ) {
+	respuesta.render("menu/new");
+});
+
+//Guardar productos
+app.post("/menu", function( solicitud, respuesta ) {
+	var data = {
+		title: solicitud.body.title,
+		descripcion: solicitud.body.descripcion,
+		imagenUrl: "",
 		precio: solicitud.body.precio
 	}
-	
+
 	var producto = new Producto(data);
-	
-	producto.save( function(error){
-			res.render("index");
+
+	producto.save( function( error ){
+		respuesta.render("index");
+	}); 
+	/*cloudinary.uploader.upload( solicitud.files.imagenUrl.path, 
+		function( result ) { 
+	  		producto.imagenUrl = result.url;
+	  		producto.save( function( error ){
+	  			console.log('producto');
+				respuesta.render("index");
+			}); 
+		}
+	);*/
+});
+
+app.get("/menu/delete/:id", function(solicitud, respuesta){
+	var id_producto = solicitud.params.id;
+	Producto.findOne({"_id": id_producto},function(error,producto){
+		respuesta.render("menu/delete",{ product: producto });
 	});
-
-	respuesta.render("menu/new");
 });
 
-app.get("/menu/new", function( solicitud, respuesta ){
-	respuesta.render("menu/new");
+app.delete("/menu/:id", function(solicitud, respuesta){
+	if(solicitud.params.id == pwd){
+		Producto.remove({"_id": solicitud.params.id}, function(error){
+			respuesta.redirect("/menu");
+		});	
+	} else {
+		respuesta.redirect("/");
+	}
 });
 
-//*Especificamos el puerto
-app.listen(8888);
+//Especificamos el puerto
+app.listen(8888);	
